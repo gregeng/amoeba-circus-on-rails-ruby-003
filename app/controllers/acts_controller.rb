59d303d96daae5cc26fc0d469763567f99b919b8
Greg Eng <greg.eng@gmail.com>
2013-11-10 00:20:11 -0500

    add edit functionality for acts

--- app/controllers/acts_controller.rb
+++ app/controllers/acts_controller.rb
@@ -32,6 +32,37 @@ class ActsController < ApplicationController
     end
   end
 
+  def edit
+    @amoebas = Amoeba.all
+    @act = Act.find(params[:id])
+  end
+
+  def update
+    amoeba_ids = params[:acts][:amoebas]
+    amoeba_objects = Amoeba.find_amoebas(amoeba_ids)
+    date = params[:act]
+    time = params[:act]
+
+    @act = Act.find(params[:act][:id])
+    @act.name = params[:act][:name]
+    @act.time = time["time(1i)"]+"-"+time["time(2i)"]+"-"+time["time(3i)"]+" "+time["time(4i)"]+":"+time["time(5i)"]
+    @act.date = date["date(1i)"]+"-"+date["date(2i)"]+"-"+date["date(3i)"]
+    @act.amoebas = amoeba_objects
+
+    if @act.save
+      redirect_to @act
+    else
+      render action: 'edit'
+    end
+  end
+
+
+  def destroy
+    @act.destroy
+
+    redirect_to acts_url
+  end
+
 
   private
     # Never trust parameters from the scary internet, only allow the white list through.
