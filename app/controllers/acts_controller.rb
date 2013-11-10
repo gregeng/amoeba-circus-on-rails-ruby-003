2696b607c64fbed54ca2adfd7b60e9d6795fae29
Greg Eng <greg.eng@gmail.com>
2013-11-10 00:12:01 -0500

    new act and show page updated

--- app/controllers/acts_controller.rb
+++ app/controllers/acts_controller.rb
@@ -13,6 +13,30 @@ class ActsController < ApplicationController
     @act = Act.new
   end
 
+  def create
+    amoeba_ids = params[:acts][:amoebas]
+    amoeba_objects = Amoeba.find_amoebas(amoeba_ids)
+    date = params[:act]
+    time = params[:act]
 
+    @act = Act.new
+    @act.name = params[:act][:name]
+    @act.time = time["time(1i)"]+"-"+time["time(2i)"]+"-"+time["time(3i)"]+" "+time["time(4i)"]+":"+time["time(5i)"]
+    @act.date = date["date(1i)"]+"-"+date["date(2i)"]+"-"+date["date(3i)"]
+    @act.amoebas = amoeba_objects
+
+    if @act.save
+      redirect_to @act
+    else
+      render action: 'new'
+    end
+  end
+
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def act_params
+      params.require(:act).permit(:name, :date, :time, :amoebas)
+    end
 
 end
--- app/models/amoeba.rb
+++ app/models/amoeba.rb
@@ -4,4 +4,15 @@ class Amoeba < ActiveRecord::Base
   has_many :acts, :through => :amoeba_acts
 
 
+
+  def self.find_amoebas(amoeba_ids)
+    if !amoeba_ids.nil?
+      amoeba_ids.collect do |amoeba_id|
+          Amoeba.find(amoeba_id)
+       end
+    else
+      []
+    end
+  end
+
 end
