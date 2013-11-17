1e58764a870e4fc232933761dd549a35434542c1
Greg Eng <greg.eng@gmail.com>
2013-11-17 13:35:44 -0500

    add some drag and drop the acts show page

---
+++ app/assets/javascripts/acts.js
@@ -0,0 +1,26 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(function() {
+  $( "#catalog li" ).draggable({
+    appendTo: "body",
+    helper: "clone"
+  });
+  $( "#cart ul" ).droppable({
+    activeClass: "ui-state-default",
+    hoverClass: "ui-state-hover",
+    accept: ":not(.ui-sortable-helper)",
+    drop: function( event, ui ) {
+      $( this ).find( ".placeholder" ).remove();
+      $( "<li></li>" ).text( ui.draggable.text() ).appendTo( this );
+    }
+  }).sortable({
+    items: "li:not(.placeholder)",
+    sort: function() {
+      // gets added unintentionally by droppable interacting with sortable
+      // using connectWithSortable fixes this, but doesn't allow you to customize active/hoverClass options
+      $( this ).removeClass( "ui-state-default" );
+    }
+  });
+});
